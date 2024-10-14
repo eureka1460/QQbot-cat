87dd411f1840efb998dbe1e6913be96a89cc2116
whm <haominwan818@gmail.com>
2024-10-14 13:07:47 +0800

    修复 Typst 渲染服务并支持跨平台使用
    - 修复了 Typst 渲染服务在不同操作系统平台上的路径问题
    - 增加了对 Typst 文本编码的检测和转换，确保在不同编码环境下也能正确渲染
    - 将渲染结果转换为 Base64 格式，使用 CQ 码发送图片，以支持更多消息类型
    - 优化了 Typst 插件的文件结构和代码组织

--- plugins/typst.py
+++ plugins/typst.py
@@ -1,16 +1,17 @@
-import typst
 import sys
 import os
 import asyncio
 import time
+import typst
 
 def render(typst_text:str)->str:
-    if sys.platform == "win32":
-        temp_file = 'tmp/' + str(time.time()) + '.typ'
+    typst_text = "#set page(width: auto, height: auto, margin: (x: 10pt, y: 10pt))\n" + typst_text
+    if sys.platform == "win32" or sys.platform == "win64":
+        temp_file = 'D:/QQbot/Bot/tmp/' + str(time.time()) + '.typ'
         try:
-            if not os.path.exists('tmp'):
-                os.makedirs('tmp')
-            with open(temp_file, 'w') as f:
+            if not os.path.exists('D:/QQbot/Bot/tmp'):
+                os.makedirs('D:/QQbot/Bot/tmp/')
+            with open(temp_file, 'w', encoding= 'utf-8') as f:
                 f.write(typst_text)
         except Exception as e:
             os.remove(temp_file)
@@ -21,7 +22,8 @@ def render(typst_text:str)->str:
             img = typst.compile(temp_file, format='png')
             os.remove(temp_file)
         except Exception as e:
-            os.remove(temp_file)
+            if os.path.exists(temp_file):
+                os.remove(temp_file)
             print("[Typst Renderer] Error:", e)
             raise e
         return img
@@ -30,7 +32,7 @@ def render(typst_text:str)->str:
         try:
             if not os.path.exists('/dev/shm'):
                 os.makedirs('/dev/shm')
-            with open(temp_file, 'w') as f:
+            with open(temp_file, 'w', encoding='utf-8') as f:
                 f.write(typst_text)
         except Exception as e:
             os.remove(temp_file)
@@ -51,3 +53,5 @@ def render(typst_text:str)->str:
 async def render_async(typst_text:str)->str:
     loop = asyncio.get_event_loop()
     return await loop.run_in_executor(None, render, typst_text)
+
+__all__ = ['render', 'render_async']
