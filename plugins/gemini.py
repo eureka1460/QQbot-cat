49deef1ad6115861ca5c664c342d3285e55045b8
Nek0 <148352727+HaominWan@users.noreply.github.com>
2024-10-16 06:30:30 +0000

    Refactor code to handle image messages and optimize image generation

--- plugins/gemini.py
+++ plugins/gemini.py
@@ -4,15 +4,29 @@ from IPython.display import Image
 import PIL.Image as pi
 import io
 import textwrap
+import aiohttp
+import os
+import time
 
 from config import *
-
+from aiohttp import ClientTimeout
 
 gai.configure(api_key=GEMINI_API_KEY)
 
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+async def url_to_image(url):
+    try:
+        async with aiohttp.ClientSession(timeout=ClientTimeout(total=10)) as session:
+            async with session.get(url) as response:
+                image = await response.read()
+                return image
+    except Exception as e:
+        print("[Gemini]Error: ",e)
+        return None
+    pass
 
 async def image_to_text(image):
     try:
